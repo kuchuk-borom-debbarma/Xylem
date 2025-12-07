cf011f41b9f3907e698285a9910a7917c8a0ed29
kuchuk-borom-debbarma <kuchukboromd@gmail.com>
2025-12-07 19:07:57 +0530

    feat: Initialize Xylem project structure with client SDK, server, and core Span data model for distributed tracing.

---
+++ src/main/java/dev/kbd/xylem/server/xylem_server/XylemServerApplication.java
@@ -0,0 +1,13 @@
+package dev.kbd.xylem.server.xylem_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XylemServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XylemServerApplication.class, args);
+	}
+
+}
